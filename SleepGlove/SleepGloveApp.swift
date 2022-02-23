37ea363f4e15dfcb7f845497dc8f71af75f6ff3a
Catherine Livingston <cel52@duke.edu>
2022-02-22 19:32:15 -0500

    Initial Commit

---
+++ SleepGlove/SleepGloveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepGloveApp.swift
+//  SleepGlove
+//
+//  Created by Catherine Livingston on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SleepGloveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
