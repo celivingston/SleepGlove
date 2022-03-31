6bd910f7fe803296e70568e6a290a3e398616325
celivingston <catherine.livingston@duke.edu>
2022-03-31 00:03:08 -0400

    Completing step 2

--- SleepGlove/StepTwoPrompt.swift
+++ SleepGlove/StepTwoPrompt.swift
@@ -7,79 +7,39 @@
 
 import SwiftUI
 struct StepTwoPrompt: View {
-    @ObservedObject var audioRecorder: AudioRecorder
-    @State private var hasRecorded = false
+    @State private var time: String = ""
+    @State private var willMoveToNextScreen = false
     var body: some View {
         VStack {
-            Text("Please record a wake up message. ")
-                .padding([.leading, .trailing], 15)
-                .padding([.top], 40)
-                .font(.system(size: 20, weight: .bold))
+            Text("Enter time until sleep.")
+                .font(.system(size: 30, weight: .bold))
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Text("Tap the Record button and read the below message. This recording will be played to wake you up from the hypnogoia sleep state. ")
-                .padding([.leading, .trailing], 15)
-                .padding([.top], 5)
-                .font(.system(size: 15, weight: .regular))
+                .padding([.leading, .trailing])
+                .padding([.bottom], 5)
+            Text("This is your best guess. Try to overestimate by a few minutes.")
                 .frame(maxWidth: .infinity, alignment: .leading)
-            Spacer()
-            Text("Press record and say the following...")
-                .font(.system(size: 15))
-            Text("\"You're falling asleep. \n Tell me what you're dreaming about.\"")
+                .padding([.leading, .trailing])
+            TextField("Enter number of minutes", text: $time)
                 .padding()
-                .background(Color.gray.opacity(0.25))
-                .cornerRadius(10)
-            if audioRecorder.recording == false {
-                Button(action: {self.audioRecorder.startRecording()}) {
-                    Image(systemName: "circle.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100)
-                        .clipped()
-                        .foregroundColor(.red)
-                        .overlay(Text("RECORD").foregroundColor(Color.white).font(.system(size: 17, weight: .bold)), alignment: .center)
-                        .shadow(radius: 10)
-                }.padding([.top, .bottom], 30)
-            } else {
-                Button(action: {
-                    self.audioRecorder.stopRecording()
-                    self.hasRecorded = true
-                }) {
-                    Image(systemName: "stop.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 100, height: 100)
-                        .clipped()
-                        .foregroundColor(.red)
-                        .overlay(Text("STOP").foregroundColor(Color.white).font(.system(size: 17, weight: .bold)), alignment: .center)
-                        .shadow(radius: 10)
-                }.padding([.top, .bottom], 30)
-            }
-            Spacer()
-            if hasRecorded {
-                Text("If there are issues that occurred during your recording, please re-record by tapping the button again. ")
-                    .font(.system(size: 15, weight: .regular))
-            }
             Button(action: {
-                if audioRecorder.recording == true {
-                    audioRecorder.stopRecording()
-                }
+                self.willMoveToNextScreen = true
             }) {
                 Text("Next")
-                    .fontWeight(.bold)
-                    .padding(20)
+                    .fontWeight(.semibold)
                     .foregroundColor(.white)
-                    .shadow(radius: 5.0)
-                    .frame(width: 200)
-            }
-                .padding()
-                .disabled(!hasRecorded)
-                .buttonStyle(MyButtonStyle())
-        }
+                    
+            }   .padding()
+                .frame(width: 200)
+                .background(Color.blue)
+                .shadow(radius: 5.0)
+                .cornerRadius(30)
+        }.textFieldStyle(.roundedBorder).navigate(to: StepThreePrompt(audioRecorder: AudioRecorder()), when: $willMoveToNextScreen)
     }
+    
 }
 
 struct StepTwoPrompt_Previews: PreviewProvider {
     static var previews: some View {
-        StepTwoPrompt(audioRecorder: AudioRecorder())
+        StepTwoPrompt()
     }
 }
